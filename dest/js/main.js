fcc49e4568e4db7a974f1727e74aa528b12859b6
Zieheavy <ryanvandenbroek@gmail.com>
2018-06-01 13:01:53 +0200

    .

--- dest/js/main.js
+++ dest/js/main.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-  $("#panel-623188").hide();
-  $("#panel-338653").hide();
-  // $("#panel-445566").hide();
-  $("#panel-556234").hide();
-=======
 $(document).ready(function() {
     $('#panel-623188').hide();
     $('#panel-338653').hide();
     $('#panel-445566').hide();
     $('#panel-556234').hide();
->>>>>>> a5944929063ed89288ecf12436df6f3e613babf3
 });
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3QuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyI8PDw8PDw8IEhFQURcclxuJChkb2N1bWVudCkucmVhZHkoZnVuY3Rpb24oKXtcclxuICAkKFwiI3BhbmVsLTYyMzE4OFwiKS5oaWRlKCk7XHJcbiAgJChcIiNwYW5lbC0zMzg2NTNcIikuaGlkZSgpO1xyXG4gIC8vICQoXCIjcGFuZWwtNDQ1NTY2XCIpLmhpZGUoKTtcclxuICAkKFwiI3BhbmVsLTU1NjIzNFwiKS5oaWRlKCk7XHJcbj09PT09PT1cclxuJChkb2N1bWVudCkucmVhZHkoZnVuY3Rpb24oKSB7XHJcbiAgICAkKCcjcGFuZWwtNjIzMTg4JykuaGlkZSgpO1xyXG4gICAgJCgnI3BhbmVsLTMzODY1MycpLmhpZGUoKTtcclxuICAgICQoJyNwYW5lbC00NDU1NjYnKS5oaWRlKCk7XHJcbiAgICAkKCcjcGFuZWwtNTU2MjM0JykuaGlkZSgpO1xyXG4+Pj4+Pj4+IGE1OTQ0OTI5MDYzZWQ4OTI4OGVjZjEyNDM2ZGY2ZjNlNjEzYmFiZjNcclxufSk7XHJcbiJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3QuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCkge1xyXG4gICAgJCgnI3BhbmVsLTYyMzE4OCcpLmhpZGUoKTtcclxuICAgICQoJyNwYW5lbC0zMzg2NTMnKS5oaWRlKCk7XHJcbiAgICAkKCcjcGFuZWwtNDQ1NTY2JykuaGlkZSgpO1xyXG4gICAgJCgnI3BhbmVsLTU1NjIzNCcpLmhpZGUoKTtcclxufSk7XHJcbiJdfQ==
--- index.php
+++ index.php
@@ -19,12 +19,13 @@
 </head>
 <body>
 <button class="btn">hallo test</button>
-    <!-- <div class="verdieping verdieping__0">
 
-<<<<<<< HEAD
-    <div class="tab-content">
-=======
-    </div> -->
+    <div class="verdieping verdieping__0">
+        <div class="L118"></div>
+        <div class="L116"></div>
+        <div class="L114"></div>
+        <div class="L113"></div>
+    </div>
     <div class="verdieping verdieping__1">
         <div class="L118"></div>
         <div class="L116"></div>
@@ -36,150 +37,6 @@
     </div>
     <div class="verdieping verdieping__3">
 
-    </div>
-    <!-- <div class="tab-content"> -->
->>>>>>> a5944929063ed89288ecf12436df6f3e613babf3
-      <div class="" id="panel-623188">
-        <div class="row">
-          <div id="plattegrond1">
-            <div id="verdieping0-1">
-              <a href="#"><div id="entrance" class="Basic2">Ingang</div></a>
-              <a href="#"><div id="L020" class="Basic">0.20</div></a>
-              <a href="#"><div id="L016" class="Basic">0.16</div></a>
-              <a href="#"><div id="L014" class="Basic">0.14</div></a>
-              <div id="opvulling1"></div>
-              <a href="#"><div id="L018" class="Basic">0.18</div></a>
-              <div id="opvulling2"></div>
-              <div id="opvulling3"></div>
-              <a href="#"><div id="L007" class="Basic">0.07</div></a>
-            </div>
-            <div id="verdieping0-2">
-              <a href="#"><div id="L010" class="Basic">0.10</div></a>
-              <a href="#"><div id="L012" class="Basic">0.12</div></a>
-              <div id="opvulling4"></div>
-              <a href="#"><div id="L004" class="Basic">0.04</div></a>
-              <a href="#"><div id="L008" class="Basic">0.08</div></a>
-              <a href="#"><div id="L006" class="Basic">0.06</div></a>
-            </div>
-            <div id="verdieping0-3" class="verdieping0-3">
-              <a href="#"><div id="L002c" class="Basic">0.02c</div></a>
-              <a href="#"><div id="L002b" class="Basic">0.02b</div></a>
-              <a href="#"><div id="L002d" class="Basic">0.02d</div></a>
-              <a href="#"><div id="L002a" class="Basic">0.02a</div></a>
-              <div id="opvulling5"></div>
-              <a href="#"><div id="L005a" class="Basic">0.05a</div></a>
-              <a href="#"><div id="L005b" class="Basic">0.05b</div></a>
-              <a href="#"><div id="L003" class="Basic">0.03</div></a>
-              <a href="#"><div id="L001e" class="Basic">0.01e</div></a>
-              <a href="#"><div id="L001d" class="Basic">0.01d</div></a>
-              <div id="opvulling6"></div>
-              <a href="#"><div id="L001a" class="Basic">0.01a</div></a>
-              <a href="#"><div id="L001b" class="Basic">0.01b</div></a>
-              <a href="#"><div id="L001" class="Basic">0.01</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class=" " id="panel-338653">
-        <div class="row">
-          <div id="plattegrond2">
-            <a href="#"><div id="L118" class="Basic">1.18</div></a>
-            <a href="#"><div id="L116" class="Basic">1.16</div></a>
-            <a href="#"><div id="L114" class="Basic">1.14</div></a>
-            <div id="opvulling7"></div>
-            <a href="#"><div id="L112" class="Basic">1.12</div></a>
-            <a href="#"><div id="L108" class="Basic">1.08</div></a>
-            <a href="#"><div id="L106" class="Basic">1.06</div></a>
-            <div id="opvulling8"></div>
-            <a href="#"><div id="L104" class="Basic">1.04</div></a>
-            <a href="#"><div id="L102" class="Basic">1.02</div></a>
-            <div id="opvulling9"></div>
-            <div id="opvulling10"></div>
-            <div id="opvulling11"></div>
-            <div id="opvulling12"></div>
-            <div id="opvulling13"></div>
-            <a href="#"><div id="L113" class="Basic">1.13</div></a>
-            <a href="#"><div id="L111" class="Basic">1.11</div></a>
-            <a href="#"><div id="L109" class="Basic">1.09</div></a>
-            <a href="#"><div id="L107" class="Basic">1.07</div></a>
-            <a href="#"><div id="L105" class="Basic">1.05</div></a>
-            <a href="#"><div id="L105a" class="Basic">1.05a</div></a>
-            <a href="#"><div id="L103" class="Basic">1.03</div></a>
-            <a href="#"><div id="L101" class="Basic">1.01</div></a>
-            <a href="#"><div id="L101a" class="Basic">1.01a</div></a>
-            <a href="#"><div id="opvulling14"></div>
-          </div>
-        </div>
-      </div>
-      <div class=" " id="panel-445566">
-        <div class="row">
-          <div id="plattegrond3">
-            <a href="#"><div id="L216" class="Basic">2.16</div></a>
-            <a href="#"><div id="L214" class="Basic">2.14</div></a>
-            <a href="#"><div id="L212" class="Basic">2.12</div></a>
-            <a href="#"><div id="L210" class="Basic">2.10</div></a>
-            <a href="#"><div id="L208" class="Basic">2.08</div></a>
-            <a href="#"><div id="L206" class="Basic">2.06</div></a>
-            <a href="#"><div id="L204" class="Basic">2.04</div></a>
-            <a href="#"><div id="L202" class="Basic">2.02</div></a>
-            <a href="#"><div id="L202a" class="Basic">2.02a</div></a>
-            <a href="#"><div id="L202b" class="Basic">2.02b</div></a>
-            <div id="opvulling15"></div>
-            <a href="#"><div id="L201a" class="Basic">2.01a</div></a>
-            <div id="opvulling16"></div>
-            <div id="opvulling17"></div>
-            <div id="opvulling18"></div>
-            <a href="#"><div id="L211" class="Basic">2.11</div></a>
-            <a href="#"><div id="L209" class="Basic">2.09</div></a>
-            <a href="#"><div id="L207" class="Basic">2.07</div></a>
-            <a href="#"><div id="L205" class="Basic">2.05</div></a>
-            <a href="#"><div id="L203" class="Basic">2.03</div></a>
-            <a href="#"><div id="L201" class="Basic">2.01</div></a>
-            <div id="opvulling19"></div>
-            <div id="opvulling20"></div>
-            <a href="#"><div id="L218b" class="Basic">2.18b</div></a>
-            <a href="#"><div id="L218a" class="Basic">2.18a</div></a>
-            <a href="#"><div id="L220" class="Basic">2.20</div></a>
-            <a href="#"><div id="L213" class="Basic">2.13</div></a>
-            <a href="#"><div id="L215" class="Basic">2.15</div></a>
-            <a href="#"><div id="L222" class="Basic">2.22</div></a>
-            <a href="#"><div id="L217" class="Basic">2.17</div></a>
-            <div id="opvulling21"></div>
-            <a href="#"><div id="L217a" class="Basic">2.17a</div></a>
-            <a href="#"><div id="L224" class="Basic">2.24</div></a>
-            <div id="opvulling22"></div>
-          </div>
-        </div>
-      </div>
-      <div class=" " id="panel-556234">
-        <div class="row">
-          <div id="plattegrond4">
-              <a href="#"><div id="L302" class="Basic">3.02</div></a>
-              <a href="#"><div id="L301" class="Basic">3.01</div></a>
-              <a href="#"><div id="L304" class="Basic">3.04</div></a>
-              <a href="#"><div id="L303" class="Basic">3.03</div></a>
-              <a href="#"><div id="L306" class="Basic">3.06</div></a>
-              <div id="opvulling23"></div>
-              <div id="opvulling24"></div>
-              <div id="opvulling25"></div>
-              <div id="opvulling26"></div>
-              <a href="#"><div id="L305" class="Basic">3.05</div></a>
-              <a href="#"><div id="L308" class="Basic">3.08</div></a>
-              <a href="#"><div id="L310" class="Basic">3.10</div></a>
-              <a href="#"><div id="L307" class="Basic">3.07</div></a>
-              <a href="#"><div id="L309" class="Basic">3.09</div></a>
-              <a href="#"><div id="L312" class="Basic">3.12</div></a>
-              <a href="#"><div id="L314" class="Basic">3.14</div></a>
-              <div id="opvulling27"></div>
-              <div id="opvulling28"></div>
-              <a href="#"><div id="L316" class="Basic">3.16</div></a>
-              <a href="#"><div id="L315" class="Basic">3.15</div></a>
-              <a href="#"><div id="L313" class="Basic">3.13</div></a>
-              <a href="#"><div id="L311" class="Basic">3.11</div></a>
-              <div id="opvulling29"></div>
-          </div>
-        </div>
-      </div>
     </div>
 
     <script src="dest/js/main.js" charset="utf-8"></script>
