873d2cc7500eb03f9e42f18523e52e5bf0ee43ed
Dylan Bos <dylanbos1996@gmail.com>
2018-06-01 11:32:32 +0200

    test commit

--- index.php
+++ index.php
@@ -11,11 +11,14 @@
     <title></title>
     <link rel="stylesheet" href="dest/css/app.css">
     <script src="dest/js/jquery.min.js"></script>
+    <script src="dest/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="dest/css/bootstrap.min.css">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+    <button class="btn">hallo test</button>
     <script src="dest/js/main.js" charset="utf-8"></script>
 </body>
 </html>
