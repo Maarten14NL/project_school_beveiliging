2588d5b7344e5d320a74edb0756df4c34d353a7c
Zieheavy <ryanvandenbroek@gmail.com>
2018-06-01 13:11:27 +0200

    .

--- index.php
+++ index.php
@@ -21,10 +21,29 @@
 <button class="btn">hallo test</button>
 
     <div class="verdieping verdieping__0">
-        <div class="L118"></div>
-        <div class="L116"></div>
-        <div class="L114"></div>
-        <div class="L113"></div>
+        <div class="ingang"></div>
+        <div class="L020"></div>
+        <div class="L018"></div>
+        <div class="L016"></div>
+        <div class="L014"></div>
+        <div class="L012"></div>
+        <div class="L010"></div>
+        <div class="L008"></div>
+        <div class="L007"></div>
+        <div class="L006"></div>
+        <div class="L005B"></div>
+        <div class="L005A"></div>
+        <div class="L004"></div>
+        <div class="L003"></div>
+        <div class="L002D"></div>
+        <div class="L002C"></div>
+        <div class="L002B"></div>
+        <div class="L002A"></div>
+        <div class="L001E"></div>
+        <div class="L001D"></div>
+        <div class="L001B"></div>
+        <div class="L001A"></div>
+        <div class="L001"></div>
     </div>
     <div class="verdieping verdieping__1">
         <div class="L118"></div>
