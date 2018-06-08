83b6b2ed76e1823d28e8401ce17b235069b704b7
Zieheavy <ryanvandenbroek@gmail.com>
2018-06-08 11:23:20 +0200

    randomsring function was not in the correct place

--- src/js/main/functions.js
+++ src/js/main/functions.js
@@ -139,37 +139,38 @@ function confirmModal(title, body, confirmClass, deleteClass){
     $('.confirm-text').text(body)
 
     $('.js-confirm').modal("show")
-    function randomString2(len, beforestr = '', arraytocheck = null) {
-        // Charset, every character in this string is an optional one it can use as a random character.
-        var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-        var randomString = '';
-        for (var i = 0; i < len; i++) {
-            // creates a random number between 0 and the charSet length. Rounds it down to a whole number
-            var randomPoz = Math.floor(Math.random() * charSet.length);
-            randomString += charSet.substring(randomPoz, randomPoz + 1);
-        }
-        // If an array is given it will check the array, and if the generated string exists in it it will create a new one until a unique one is found *WATCH OUT. If all available options are used it will cause a loop it cannot break out*
-        if (arraytocheck == null) {
+}
+
+function randomString2(len, beforestr = '', arraytocheck = null) {
+    // Charset, every character in this string is an optional one it can use as a random character.
+    var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+        // creates a random number between 0 and the charSet length. Rounds it down to a whole number
+        var randomPoz = Math.floor(Math.random() * charSet.length);
+        randomString += charSet.substring(randomPoz, randomPoz + 1);
+    }
+    // If an array is given it will check the array, and if the generated string exists in it it will create a new one until a unique one is found *WATCH OUT. If all available options are used it will cause a loop it cannot break out*
+    if (arraytocheck == null) {
+        return beforestr + randomString;
+    } else {
+        var isIn = $.inArray(beforestr + randomString, arraytocheck); // checks if the string is in the array, returns a position
+        if (isIn > -1) {
+            // if the position is not -1 (meaning, it is not in the array) it will start doing a loop
+            var count = 0;
+            do {
+                randomString = '';
+                for (var i = 0; i < len; i++) {
+                    var randomPoz = Math.floor(Math.random() * charSet.length);
+                    randomString += charSet.substring(randomPoz, randomPoz + 1);
+                }
+                isIn = $.inArray(beforestr + randomString, arraytocheck);
+                count++;
+            } while (isIn > -1);
+            console.log('it took ' + count + ' tries');
             return beforestr + randomString;
         } else {
-            var isIn = $.inArray(beforestr + randomString, arraytocheck); // checks if the string is in the array, returns a position
-            if (isIn > -1) {
-                // if the position is not -1 (meaning, it is not in the array) it will start doing a loop
-                var count = 0;
-                do {
-                    randomString = '';
-                    for (var i = 0; i < len; i++) {
-                        var randomPoz = Math.floor(Math.random() * charSet.length);
-                        randomString += charSet.substring(randomPoz, randomPoz + 1);
-                    }
-                    isIn = $.inArray(beforestr + randomString, arraytocheck);
-                    count++;
-                } while (isIn > -1);
-                console.log('it took ' + count + ' tries');
-                return beforestr + randomString;
-            } else {
-                return beforestr + randomString;
-            }
+            return beforestr + randomString;
         }
     }
 }
