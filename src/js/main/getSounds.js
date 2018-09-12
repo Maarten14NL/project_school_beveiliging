68bb79daf649b83478e230fac19e07214fa17bce
Dylan Bos <dylanbos1996@gmail.com>
2018-09-12 12:28:33 +0200

    files works

--- src/js/main/getSounds.js
+++ src/js/main/getSounds.js
@@ -3,8 +3,10 @@ function getfiles() {
         if (status == 'success') {
             // alert(response);
             var decoded = JSON.parse(response);
-            console.log(response);
             files = decoded;
+            files.map(function(elem, index) {
+                $('#scenario-newsound').append('<option value="' + elem + '">'+elem+'</option>');
+            })
         }
     });
 }
