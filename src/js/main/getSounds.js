312513557cb1ee75f0a5ac370805161304dd4bcb
Zieheavy <ryanvandenbroek@gmail.com>
2018-12-05 14:25:11 +0100

    Geen sound optie toegeveogd

--- src/js/main/getSounds.js
+++ src/js/main/getSounds.js
@@ -4,6 +4,7 @@ function getfiles() {
             // alert(response);
             var decoded = JSON.parse(response);
             files = decoded;
+            $('.js-new-scenario-sound').append('<option value="#">Geen Geluid</option>');
             files.map(function(elem, index) {
                 $('.js-new-scenario-sound').append('<option value="' + elem + '">'+elem+'</option>');
             })
